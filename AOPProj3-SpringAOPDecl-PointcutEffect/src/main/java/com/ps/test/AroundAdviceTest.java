f1c0fd39d9fe89f8d970e2f277619bdda475cb7e
Lenovo <Lenovo@DESKTOP-NHBQ8G6>
2020-07-12 19:16:00 +0530

    SpringAOP-PointcutEffect

---
+++ AOPProj3-SpringAOPDecl-PointcutEffect/src/main/java/com/ps/test/AroundAdviceTest.java
@@ -0,0 +1,38 @@
+package com.ps.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ps.service.BankService;
+
+public class AroundAdviceTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=null;
+		BankService proxy=null;
+		
+		//get IOC container
+		ctx=new ClassPathXmlApplicationContext("com/ps/cfgs/applicationContext.xml");
+		//call bean to proxy class object
+		proxy=ctx.getBean("pfb", BankService.class);
+		System.out.println("proxy object classname "+proxy.getClass()+"  super class "+proxy.getClass().getSuperclass());
+		try {
+			System.out.println("1 Simple Interest Amount :: "+proxy.calcSimpleInterestAmount(10000, 2, 12));
+			System.out.println(".........................................................................................");
+			System.out.println("1 Compound Interst Amount :: "+proxy.calcCompoundInterestAmount(10000, 2, 12));
+			System.out.println("=================================================================");
+			System.out.println("2 Simple Interest Amount :: "+proxy.calcSimpleInterestAmount(10000, 2, 12));
+			System.out.println(".........................................................................................");
+			System.out.println("2 Compound Interst Amount :: "+proxy.calcCompoundInterestAmount(10000, 2, 12));
+		
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		//close container
+		((AbstractApplicationContext) ctx).close();
+	}//main
+
+}//class
