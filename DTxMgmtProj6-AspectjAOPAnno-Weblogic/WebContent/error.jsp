89588013094a67573b42803070b9407d5db9f7d2
Lenovo <Lenovo@DESKTOP-NHBQ8G6>
2020-08-15 08:17:58 +0530

    DistributedTx-WebLogicServer

---
+++ DTxMgmtProj6-AspectjAOPAnno-Weblogic/WebContent/error.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1" isELIgnored="false"%>
+
+    
+    <h1 style="color:red;text-align:center">Error page</h1><br>
+    
+    <b>Error : ${errorMsg } </b><br>
+    
+    <a href="form.html">home</a>
---
+++ DTxMgmtProj6-AspectjAOPAnno-Weblogic/src/main/java/com/ps/controller/MainControllerServlet.java
@@ -0,0 +1,63 @@
+package com.ps.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ps.service.BankMgmtService;
+
+@WebServlet("/controller")
+public class MainControllerServlet extends HttpServlet {
+	private ApplicationContext ctx;
+       
+	public void init() throws ServletException {
+		ctx=new ClassPathXmlApplicationContext("com/ps/cfgs/applicationContext.xml");
+	}
+
+	public void destroy() {
+		((AbstractApplicationContext) ctx).close();
+	}
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		long srcNo=0L,destNo=0L;
+		float amt=0.0f;
+		BankMgmtService service=null;
+		String result=null;
+		RequestDispatcher rd=null;
+		
+		//read form data
+		srcNo=Long.parseLong(req.getParameter("srcAcno"));
+		destNo=Long.parseLong(req.getParameter("destAcno"));
+		amt=Float.parseFloat(req.getParameter("amt"));
+		//get Service class object
+		service=ctx.getBean("bankService", BankMgmtService.class);
+		try {
+			//invoke method
+			result=service.transferMoney(srcNo, destNo, amt);
+			req.setAttribute("resultMsg", result);
+			//forward to result.jsp page
+			rd=req.getRequestDispatcher("/result.jsp");
+			rd.forward(req, resp);
+		}
+		catch(Exception e) {
+			req.setAttribute("errorMsg", e.getMessage());
+			rd=req.getRequestDispatcher("/error.jsp");
+			rd.forward(req, resp);
+			e.printStackTrace();
+		}
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+}
